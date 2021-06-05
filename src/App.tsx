e5d91756a04597b97c215234203aa53268370b7b
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-06-05 15:48:32 +0900

    chore: init firebaseApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/app";
+
+import { db } from "./firebaseApp";
+
+type Todo = {
+  id: string;
+  ref: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>;
+  name: string;
+};
 
 function App() {
+  const todosRef = db.collection("todos");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = todosRef.onSnapshot((snap) => {
+      const newTodos = snap.docs.map((doc) => ({
+        id: doc.id,
+        ref: doc.ref,
+        ...doc.data(),
+      })) as Todo[];
+      setTodos(newTodos);
+    });
+    return unsubscribe;
+  }, [todosRef]);
+
+  const addTodo = async () => {
+    await todosRef.add({ name });
+    setName("");
+  };
+
+  const [name, setName] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo();
+        }}
+      >
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <div style={{ display: "flex", padding: "0.5rem 0" }}>
+              <div style={{ marginRight: "1rem" }}>{todo.name}</div>
+              <button onClick={() => todo.ref.delete()}>remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/firebaseApp.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKZpdYcX5iTLqoNW5XMSEac02jDyr5yLE",
+  authDomain: "playground-67a20.firebaseapp.com",
+  projectId: "playground-67a20",
+  storageBucket: "playground-67a20.appspot.com",
+  messagingSenderId: "1008914470267",
+  appId: "1:1008914470267:web:77fc11d8586398920bd9de",
+  measurementId: "G-P2RHN4186V",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
