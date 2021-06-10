215dd776dbc7b1daa58601f3b20c0b77468892be
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-06-10 23:55:02 +0900

    fix a bit

--- seed/set-todos.js
+++ seed/set-todos.js
@@ -2,7 +2,7 @@ const { todosRef } = require("./index");
 const faker = require("faker");
 
 const setTodos = async () => {
-  for (let i = 0; i < 99; i++) {
+  for (let i = 10; i < 30; i++) {
     await todosRef.add({ name: i.toString().padStart(2, 0) });
   }
 };
